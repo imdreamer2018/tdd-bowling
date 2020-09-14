a3490b6c2e74218f4ad27935a04f11a148d26039
imdreamer <743295483@qq.com>
2020-09-14 21:11:08 +0800

    [yangqian] implement: should throw invalid input exception when score rounds less than 10 times.

--- src/main/java/BowlingGame.java
+++ src/main/java/BowlingGame.java
@@ -6,28 +6,35 @@ public class BowlingGame {
     public int calculateTotalScore(List<Integer> scoreOfKnockingDownTheBall) {
 
         int totalScore = 0;
-        int currentFrame = 0;
+        int currentRounds = 0;
+
         for (int i = 0; i < scoreOfKnockingDownTheBall.size(); i++) {
-            if (currentFrame == 10) {
+
+            if (currentRounds == 10) {
                 break;
             }
+
+            if (i + 1 >= scoreOfKnockingDownTheBall.size() || i + 2 >= scoreOfKnockingDownTheBall.size()) {
+                throw new InvalidInputException("the times of rounds less than 10");
+            }
+
             if (scoreOfKnockingDownTheBall.get(i) > 10) {
                 throw new InvalidInputException("score more than 10 points in once hit");
             } else if (scoreOfKnockingDownTheBall.get(i) == 10) {
                 totalScore += (10 +
                         scoreOfKnockingDownTheBall.get(i + 1) +
                         scoreOfKnockingDownTheBall.get(i + 2));
-                currentFrame++;
+                currentRounds++;
             } else if (scoreOfKnockingDownTheBall.get(i) + scoreOfKnockingDownTheBall.get(i + 1) > 10) {
                 throw new InvalidInputException("score more than 10 points in twice hit");
             } else if (scoreOfKnockingDownTheBall.get(i) + scoreOfKnockingDownTheBall.get(i + 1) == 10) {
                 totalScore += (10 + scoreOfKnockingDownTheBall.get(i + 2));
-                currentFrame++;
+                currentRounds++;
                 i++;
             } else {
                 totalScore += (scoreOfKnockingDownTheBall.get(i) + scoreOfKnockingDownTheBall.get(i + 1));
                 i++;
-                currentFrame++;
+                currentRounds++;
             }
         }
         return totalScore;
--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -82,4 +82,17 @@ public class BowlingGameTest {
         });
 
     }
+
+    @Test
+    void should_throw_invalid_input_exception_when_score_rounds_less_than_10_times() {
+
+        BowlingGame bowlingGame = new BowlingGame();
+
+        List<Integer> scoreOfKnockingDownTheBall = Arrays.asList(4,4,10,4,6,4,3,4,6,4,6,2,4,6,4,6);
+
+        Assertions.assertThrows(InvalidInputException.class, () -> {
+            bowlingGame.calculateTotalScore(scoreOfKnockingDownTheBall);
+        });
+
+    }
 }
